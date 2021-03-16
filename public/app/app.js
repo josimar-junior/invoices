8cc38d13172abbe80d03a8f830250ca7f7428e3d
josimar-junior <juniorpaz99@gmail.com>
2021-03-15 23:28:34 -0300

    Using takeUntil and debounceTime

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,20 @@
-import {log} from './utils/promise-helpers.js'
 import './utils/array-helpers.js'
+import {log} from './utils/promise-helpers.js'
 import {invoiceService} from './invoice/service.js'
+import {takeUntil, debounceTime, partialize, pipe} from "./utils/operators.js";
 
-document.getElementById("myButton")
-    .onclick = () => 
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+)
+
+const action = operations(() => 
     invoiceService
     .sumItems('2143')
     .then(log)
     .catch(log)
+)
+
+document.getElementById("myButton")
+    .onclick = action; 
+    
